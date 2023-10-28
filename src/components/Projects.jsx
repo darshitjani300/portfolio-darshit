7848f2c5fd6ecbbc6d5af49bc505e66db8bd7e10
darshit <janidarshit300@gmail.com>
2023-10-28 08:43:17 +0530

    Did some ui changes

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,10 +2,14 @@ import React from "react";
 
 const Projects = () => {
   return (
-    <div className="container flex flex-col py-24">
-      <div>
-        <h1 className="text-[13px] font-bold text-[#147EFB]">PORTFOLIO</h1>
-        <h2 className="font-bold text-[#2d2e32] tracking-wide">Each project is a unique piece of development 🧩</h2>
+    <div className="container flex flex-col py-5 px-2">
+      <div className="flex flex-col justify-center items-center gap-3 text-center">
+        <h1 className="text-[1rem] font-bold text-[#147EFB] leading-5">
+          PORTFOLIO
+        </h1>
+        <h2 className="text-[1.6rem] font-bold leading-9">
+          Each project is a unique piece of development 🧩
+        </h2>
       </div>
     </div>
   );
