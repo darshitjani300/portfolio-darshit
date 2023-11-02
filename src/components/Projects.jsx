4c5764e789a587110c8032cd6f5c69ab6396a661
darshit <janidarshit300@gmail.com>
2023-11-02 12:10:10 +0530

    Added another project

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -4,7 +4,7 @@ import { FiGithub } from "react-icons/fi";
 
 const Projects = () => {
   return (
-    <div className="flex flex-col py-5 px-2 sm:px-12 xl:px-52 ">
+    <div className="flex flex-col py-5 px-2 sm:px-12 md:px-20 lg:px-12 xl:px-52 ">
       <div className="flex flex-col sm:items-start justify-center items-center gap-3 text-center">
         <h1 className="text-[1rem] font-bold text-[#147EFB] leading-5">
           PORTFOLIO
@@ -13,8 +13,8 @@ const Projects = () => {
           Each project is a unique piece of development 🧩
         </h2>
         <div className="px-3 md:px-0 mt-10">
-          <div className="grid bg-white rounded-xl shadow-xl grid-rows-2 xl:grid-rows-1 grid-cols-1">
-            <div className="flex flex-col xl:flex-row gap-2 p-4">
+          <div className="grid bg-white rounded-xl shadow-xl sm:gap-10 grid-rows-2 xl:grid-rows-1 grid-cols-1">
+            <div className="flex flex-col lg:grid lg:grid-cols-2 lg:items-center gap-2 p-4 sm:p-10">
               <div className="w-full">
                 <img
                   src="car-rental.png"
@@ -22,7 +22,7 @@ const Projects = () => {
                   alt="car-rental"
                 />
               </div>
-              <div className="flex flex-col text-center items-center gap-5 pt-10">
+              <div className="flex flex-col text-center lg:px-10 items-center gap-5 pt-10 lg:pt-0">
                 <h1 className="font-bold text-[1.1rem]">
                   CAR RENTAL{" "}
                   <span className="text-[hsla(0,5%,8%,.773)] text-[0.9rem]">
@@ -51,26 +51,23 @@ const Projects = () => {
                 </div>
               </div>
             </div>
-            <div className="flex flex-col xl:flex-row gap-2 p-4">
+            <div className="flex flex-col lg:grid lg:grid-cols-2 items-center justify-center gap-2 p-4 sm:p-10">
               <div className="w-full">
                 <img
-                  src="car-rental.png"
+                  src="gym-ecom.png"
                   className="rounded-xl shadow-lg"
-                  alt="car-rental"
+                  alt="Gym-Ecomm"
                 />
               </div>
-              <div className="flex flex-col text-center items-center gap-5 pt-10">
+              <div className="flex flex-col lg:w-[80%] lg:mx-auto text-center items-center gap-5 pt-10 lg:pt-0">
                 <h1 className="font-bold text-[1.1rem]">
-                  CAR RENTAL{" "}
+                  GYMNITE {" "}
                   <span className="text-[hsla(0,5%,8%,.773)] text-[0.9rem]">
                     (SEPTEMBER 2023)
                   </span>
                 </h1>
                 <p className="text-[#767676] font-medium tracking-wide px-2">
-                  A car rental website is an online platform that allows users
-                  to rent cars for personal or business use. The website
-                  provides an interface for searching, comparing, and reserving
-                  cars.
+                A GYMNITE website, with its focus on simplicity and clean design, along with a touch of Swipers, is a comprehensive resource for fitness information and products. This ensures an easy user experience and allows customers to find and purchase the products they need.
                 </p>
                 <div className="flex items-center justify-center gap-5">
                   <button className=" p-2 font-bold shadow-md">React</button>
